b81d65ebb4f4e77df66e5311111dd138dc067375
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-07-27 11:57:43 +0700

    feat : navbar, sidebar, and its responsive components

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from '@clerk/nextjs';
-
 const DashboardPage = () => {
     return (
         <div>
             <p>This is dashboard instead</p>
-            <UserButton afterSignOutUrl="/" />
         </div>
     )
 }
